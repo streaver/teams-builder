c6673514784bec472605d66cb2ee9c9f743d0794
Juan Larrambebere <juanlarrambebere@192.168.0.105>
2023-03-01 14:21:41 -0300

    The teams and team-members data is retrieved from the local storage insted of the file system

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import { Bench } from "@/components/Bench";
 import { AuthModal } from "@/components/sign-in/AuthModal";
 import { RecoilProvider } from "@/providers/RecoilProvider";
-import { promises as fs } from "fs";
-import { GetServerSideProps } from "next";
 import dynamic from "next/dynamic";
 import Head from "next/head";
 import { useState } from "react";
@@ -14,12 +12,7 @@ const DynamicInfiniteCanvas = dynamic(
   }
 );
 
-type Props = {
-  teamsJson: string;
-  teamMembersJson: string;
-};
-
-export default function Home({ teamsJson, teamMembersJson }: Props) {
+export default function Home() {
   const [showModal, setShowModal] = useState(false);
 
   return (
@@ -40,10 +33,7 @@ export default function Home({ teamsJson, teamMembersJson }: Props) {
             />
             <link rel="icon" href="/dam.png" />
           </Head>
-          <RecoilProvider
-            teams={JSON.parse(teamsJson)}
-            teamMembers={JSON.parse(teamMembersJson)}
-          >
+          <RecoilProvider>
             <div className="flex w-full h-full">
               <DynamicInfiniteCanvas />
               <div className="w-1/5 shrink-0" />
@@ -55,20 +45,3 @@ export default function Home({ teamsJson, teamMembersJson }: Props) {
     </>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const rootDirectory = process.cwd() + "/src";
-
-  const teamsJson = await fs.readFile(rootDirectory + "/teams.json", "utf8");
-  const teamMembersJson = await fs.readFile(
-    rootDirectory + "/team-members.json",
-    "utf8"
-  );
-
-  return {
-    props: {
-      teamsJson,
-      teamMembersJson,
-    },
-  };
-};
