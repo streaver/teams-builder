1c6ccd2ca46bd14a30e55fd194a0da09790b9df5
Juan Larrambebere <juanlarrambebere@Juans-MacBook-Pro.local>
2023-03-07 13:44:17 -0300

    Hide the original avatar while being dragged

--- src/components/TeamMemberAvatar.tsx
+++ src/components/TeamMemberAvatar.tsx
@@ -12,22 +12,24 @@ export const TeamMemberAvatar = ({ id }: Props) => {
 
   const shortName = `${teamMember.firstName} ${teamMember.lastName[0]}.`;
 
-  const [_, dragRef] = useTeamMemberDrag(id);
+  const [{ isDragging }, dragRef] = useTeamMemberDrag(id);
 
   return (
-    <div
-      ref={dragRef}
-      className="relative w-16 h-16 overflow-hidden bg-white rounded-full group"
-    >
-      <Image
-        fill
-        alt={`${teamMember.firstName} ${teamMember.lastName}`}
-        src={teamMember.picture}
-        className="object-contain rounded-full group-hover:opacity-60 hover:ring-2 ring-dam-gray-600"
-      />
-      <p className="absolute hidden w-full text-xs text-center truncate -translate-x-1/2 -translate-y-1/2 text-dam-gray-600 left-1/2 top-1/2 group-hover:block ">
-        {shortName}
-      </p>
-    </div>
+    !isDragging && (
+      <div
+        ref={dragRef}
+        className="relative w-16 h-16 overflow-hidden bg-white rounded-full group"
+      >
+        <Image
+          fill
+          alt={`${teamMember.firstName} ${teamMember.lastName}`}
+          src={teamMember.picture}
+          className="object-contain rounded-full group-hover:opacity-60 hover:ring-2 ring-dam-gray-600"
+        />
+        <p className="absolute hidden w-full text-xs text-center truncate -translate-x-1/2 -translate-y-1/2 text-dam-gray-600 left-1/2 top-1/2 group-hover:block ">
+          {shortName}
+        </p>
+      </div>
+    )
   );
 };
