9eadc8f5d17c7a481f39de665c5dd21769ca1ba8
SBentancourt <sbentancourt.github@gmail.com>
2023-03-07 12:14:23 -0300

    Member boxes can fit in the Team Box without any space between them.

--- src/components/TeamBox.tsx
+++ src/components/TeamBox.tsx
@@ -3,8 +3,10 @@ import CanvasStore from "@/state/CanvasStore";
 import { teamAtomFamily } from "@/state/recoil/atoms/teamAtomFamily";
 import { teamBoxAtomFamily } from "@/state/recoil/atoms/teamBoxAtomFamily";
 import { teamMembersSelectorFamily } from "@/state/recoil/selectors/teamMembersSelectorFamily";
+import { TeamMember } from "@/types/Team";
 import { TEAM_PADDING } from "@/utils/constants";
 import { inBounds } from "@/utils/math-utils";
+import { useMemo } from "react";
 import { useRecoilValue } from "recoil";
 import { TeamBoxWrapper } from "./TeamBoxWrapper";
 import { TeamMemberBox } from "./TeamMemberBox";
@@ -16,12 +18,34 @@ type Props = {
 export const TeamBox = ({ id }: Props) => {
   const teamBox = useRecoilValue(teamBoxAtomFamily(id));
   const team = useRecoilValue(teamAtomFamily(id));
-  const teamMembers = useRecoilValue(teamMembersSelectorFamily(id));
+  const teamMembers = [...useRecoilValue(teamMembersSelectorFamily(id))].sort(
+    (member1, member2) => member2.hours - member1.hours
+  );
 
   const screen = CanvasStore.screen;
   const isInScreen = inBounds(teamBox, screen);
 
   const [_, teamDragRef] = useTeamDrag(id);
+
+  const [membersLeftCol, membersRightCol] = useMemo(() => {
+    const leftCol: TeamMember[] = [];
+    const rightCol: TeamMember[] = [];
+
+    let totalHoursLeftCol = 0;
+    let totalHoursRightCol = 0;
+
+    teamMembers.forEach((member) => {
+      if (totalHoursLeftCol <= totalHoursRightCol) {
+        leftCol.push(member);
+        totalHoursLeftCol += member.hours;
+      } else {
+        rightCol.push(member);
+        totalHoursRightCol += member.hours;
+      }
+    });
+
+    return [leftCol, rightCol];
+  }, [teamMembers]);
 
   return isInScreen ? (
     <TeamBoxWrapper id={id}>
@@ -39,13 +63,26 @@ export const TeamBox = ({ id }: Props) => {
           className="relative flex flex-wrap h-full gap-2"
           style={{ padding: TEAM_PADDING }}
         >
-          {teamMembers.map((member) => (
-            <TeamMemberBox
-              key={member.id}
-              id={member.id}
-              hours={member.hours}
-            />
-          ))}
+          <div className="grid grid-cols-2 gap-2">
+            <div className="flex flex-col gap-2">
+              {membersLeftCol.map((member) => (
+                <TeamMemberBox
+                  key={member.id}
+                  id={member.id}
+                  hours={member.hours}
+                />
+              ))}
+            </div>
+            <div className="flex flex-col gap-2">
+              {membersRightCol.map((member) => (
+                <TeamMemberBox
+                  key={member.id}
+                  id={member.id}
+                  hours={member.hours}
+                />
+              ))}
+            </div>
+          </div>
         </div>
         <span className="absolute -translate-x-1/2 left-1/2 -bottom-8">
           {team.name}
