f783eadfa1dddccf0390ff4f65e6b99853f505c4
Juan Larrambebere <juanlarrambebere@192.168.0.104>
2023-03-09 11:18:21 -0300

    Use constant

--- src/components/team-box/NewTeamDragPreview.tsx
+++ src/components/team-box/NewTeamDragPreview.tsx
@@ -1,15 +1,6 @@
 import { TeamMember } from "@/types/Team";
-import {
-  MEMBER_BORDER,
-  MEMBER_WIDTH,
-  TEAM_GAP,
-  TEAM_PADDING,
-} from "@/utils/constants";
+import { DEFAULT_TEAM_BOX_HEIGHT } from "@/utils/constants";
 import { TeamMemberBox } from "../team-member/TeamMemberBox";
-
-// TODO move to constants
-const TEAM_BOX_DEFAULT_SIZE =
-  2 * (MEMBER_WIDTH + TEAM_PADDING + MEMBER_BORDER) + TEAM_GAP;
 
 type Props = {
   teamMemberId: TeamMember["id"];
@@ -21,8 +12,8 @@ export const NewTeamDragPreview = ({ teamMemberId }: Props) => {
       <div
         className="absolute z-10 flex flex-col items-center justify-center gap-1 text-4xl border-2 border-dashed -top-2 -left-2 border-dam-gray-400 bg-white/60 rounded-3xl text-dam-gray-400"
         style={{
-          height: TEAM_BOX_DEFAULT_SIZE,
-          width: TEAM_BOX_DEFAULT_SIZE,
+          height: DEFAULT_TEAM_BOX_HEIGHT,
+          width: DEFAULT_TEAM_BOX_HEIGHT,
         }}
       >
         <p>Drop</p>
