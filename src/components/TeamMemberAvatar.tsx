d432ab292f207d4de23b705691aa6b5f91d7db8e
Juan Larrambebere <juanlarrambebere@192.168.1.39>
2023-03-09 11:25:23 -0300

    fix typescript

--- src/components/TeamMemberAvatar.tsx
+++ src/components/TeamMemberAvatar.tsx
@@ -14,22 +14,20 @@ export const TeamMemberAvatar = ({ id }: Props) => {
 
   const [{ isDragging }, dragRef] = useTeamMemberDrag(id);
 
-  return (
-    !isDragging && (
-      <div
-        ref={dragRef}
-        className="relative w-16 h-16 overflow-hidden bg-white rounded-full group"
-      >
-        <Image
-          fill
-          alt={`${teamMember.firstName} ${teamMember.lastName}`}
-          src={teamMember.picture}
-          className="object-contain rounded-full group-hover:opacity-60 hover:ring-2 ring-dam-gray-600"
-        />
-        <p className="absolute hidden w-full text-xs text-center truncate -translate-x-1/2 -translate-y-1/2 text-dam-gray-600 left-1/2 top-1/2 group-hover:block ">
-          {shortName}
-        </p>
-      </div>
-    )
-  );
+  return isDragging ? (
+    <div
+      ref={dragRef}
+      className="relative w-16 h-16 overflow-hidden bg-white rounded-full group"
+    >
+      <Image
+        fill
+        alt={`${teamMember.firstName} ${teamMember.lastName}`}
+        src={teamMember.picture}
+        className="object-contain rounded-full group-hover:opacity-60 hover:ring-2 ring-dam-gray-600"
+      />
+      <p className="absolute hidden w-full text-xs text-center truncate -translate-x-1/2 -translate-y-1/2 text-dam-gray-600 left-1/2 top-1/2 group-hover:block ">
+        {shortName}
+      </p>
+    </div>
+  ) : null;
 };
