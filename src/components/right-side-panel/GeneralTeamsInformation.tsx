dfeaf1d01991b5eb26787a8b5c5fb5f9cee0137b
Juan Larrambebere <juanlarrambebere@192.168.1.39>
2023-03-13 12:46:40 -0300

    Display the number of full times, part times, quart times & the bench info

--- src/components/right-side-panel/GeneralTeamsInformation.tsx
+++ src/components/right-side-panel/GeneralTeamsInformation.tsx
@@ -1,4 +1,6 @@
 import { teamMembersByWorkingHoursSelectorFamily } from "@/state/recoil/selectors/teamMembersByWorkingHoursSelectorFamily";
+import { teamMembersSelectorFamily } from "@/state/recoil/selectors/teamMembersSelectorFamily";
+import Fraction from "fraction.js";
 import { useRecoilValue } from "recoil";
 
 export const GeneralTeamsInformation = () => {
@@ -12,12 +14,43 @@ export const GeneralTeamsInformation = () => {
     teamMembersByWorkingHoursSelectorFamily(4)
   );
 
+  const numOfFullTimesFromPartials = Math.floor(
+    (partTimeMembers.length * 6 + quartTimemembers.length * 4) / 8
+  );
+
+  const benchedMembers = useRecoilValue(teamMembersSelectorFamily(null));
+  const benchWorkingHours = benchedMembers.reduce((workingHours, teamMeber) => {
+    return workingHours + teamMeber.hours;
+  }, 0);
+
+  const benchFullTimes = Math.floor(benchWorkingHours / 8);
+  const benchFullTimesFromPartials = new Fraction(
+    (benchWorkingHours % 8) / 8
+  ).toFraction();
+
+  const benchFullTimesText = `${benchFullTimes > 0 ? benchFullTimes : ""} ${
+    benchFullTimesFromPartials !== "0" ? ` ${benchFullTimesFromPartials}` : ""
+  }`;
+
   return (
-    <div className="flex flex-col items-center gap-4">
-      <h1 className="p-4 text-sm font-bold">Engineering team</h1>
-      <p>{fullTimeMembers.length} Full-Time</p>
-      <p>{partTimeMembers.length} Part-Time</p>
-      <p>{quartTimemembers.length} Quart-Time</p>
+    <div className="flex flex-col items-stretch gap-4 p-4 text-sm divide-y">
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="font-bold">Engineering team</h1>
+        <p>{fullTimeMembers.length} Full-Time</p>
+        <p>{partTimeMembers.length} Part-Time</p>
+        <p>{quartTimemembers.length} Quart-Time</p>
+        <p className="font-medium ">
+          means {numOfFullTimesFromPartials} Full-Time
+        </p>
+      </div>
+      <div className="flex flex-col items-center gap-4 pt-4">
+        <p>
+          Bench size: {benchedMembers.length}{" "}
+          {benchedMembers.length > 0
+            ? `(${benchFullTimesText} Full-Time)`
+            : null}
+        </p>
+      </div>
     </div>
   );
 };
