2644438eddce53e8c7944e99c1cfc80b17d77da6
Juan Larrambebere <juanlarrambebere@192.168.1.39>
2023-03-13 11:33:21 -0300

    Transformed the previous selectorinto a family so we can get the members by working hours

--- src/components/right-side-panel/GeneralTeamsInformation.tsx
+++ src/components/right-side-panel/GeneralTeamsInformation.tsx
@@ -1,13 +1,23 @@
-import { fullTimeMembersSelector } from "@/state/recoil/selectors/fullTimeMembersSelector";
+import { teamMembersByWorkingHoursSelectorFamily } from "@/state/recoil/selectors/teamMembersByWorkingHoursSelectorFamily";
 import { useRecoilValue } from "recoil";
 
 export const GeneralTeamsInformation = () => {
-  const fullTimeMembers = useRecoilValue(fullTimeMembersSelector);
+  const fullTimeMembers = useRecoilValue(
+    teamMembersByWorkingHoursSelectorFamily(8)
+  );
+  const partTimeMembers = useRecoilValue(
+    teamMembersByWorkingHoursSelectorFamily(6)
+  );
+  const quartTimemembers = useRecoilValue(
+    teamMembersByWorkingHoursSelectorFamily(4)
+  );
 
   return (
     <div className="flex flex-col items-center gap-4">
       <h1 className="p-4 text-sm font-bold">Engineering team</h1>
       <p>{fullTimeMembers.length} Full-Time</p>
+      <p>{partTimeMembers.length} Part-Time</p>
+      <p>{quartTimemembers.length} Quart-Time</p>
     </div>
   );
 };
--- src/types/Team.d.ts
+++ src/types/Team.d.ts
@@ -6,11 +6,13 @@ export type Team = {
   clientId: Client["id"] | null;
 };
 
+export type WorkingHours = 8 | 6 | 4;
+
 export type TeamMember = {
   id: number;
   firstName: string;
   lastName: string;
   picture: string;
   teamId: Team["id"] | null; // null means benched
-  hours: number;
+  hours: WorkingHours;
 };
